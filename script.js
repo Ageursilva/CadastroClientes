e587f46e4b226ad23c7bb2be4c59b69a62d211a4
Ageu Silva <ageu.tecnologia@gmail.com>
2023-04-15 12:05:07 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,89 @@
+
+function limpa_formulário_cep() {
+        //Limpa valores do formulário de cep.
+        document.getElementById('rua').value=("");
+        document.getElementById('bairro').value=("");
+        document.getElementById('cidade').value=("");
+        document.getElementById('uf').value=("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('rua').value=(conteudo.logradouro);
+        document.getElementById('bairro').value=(conteudo.bairro);
+        document.getElementById('cidade').value=(conteudo.localidade);
+        document.getElementById('uf').value=(conteudo.uf);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+    
+function pesquisacep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('rua').value="...";
+            document.getElementById('bairro').value="...";
+            document.getElementById('cidade').value="...";
+            document.getElementById('uf').value="...";
+        
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpa_formulário_cep();
+    }
+};
+
+function gerarJSON() {
+    const form = document.querySelector('.cadastro');
+    const json = {
+      nome: form.name.value,
+      sobrenome: form.surname.value,
+      cpfCnpj: form.cpfCnpj.value,
+      email: form.email.value,
+      telefone: form.telephone.value,
+      cep: form.cep.value,
+      rua: form.rua.value,
+      numero: form.number.value,
+      bairro: form.bairro.value,
+      cidade: form.cidade.value,
+      uf: form.uf.value
+    };
+  
+    localStorage.setItem('cadastro', JSON.stringify(json));
+    console.log(localStorage);
+  
+    form.reset();
+  
+    alert('Cadastro com sucesso!');
+  }

fed4d104c7b9a134243927ee9e28656265400160
Ageu Silva <39139593+Ageursilva@users.noreply.github.com>
2023-05-01 15:22:01 -0300

    refatoração
    Feito a refatoração no código, dividido cada função em um js diferente

---
+++ script/gerarJson.js
@@ -0,0 +1,27 @@
+
+function gerarJSON(event) {
+  event.preventDefault();
+  const form = document.querySelector('.cadastro');
+  const json = {
+    nome: form.name.value,
+    sobrenome: form.surname.value,
+    cpfCnpj: form.cpfCnpj.value,
+    email: form.email.value,
+    telefone: form.telephone.value,
+    cep: form.cep.value,
+    rua: form.rua.value,
+    numero: form.number.value,
+    bairro: form.bairro.value,
+    cidade: form.cidade.value,
+    uf: form.uf.value
+  };
+
+  localStorage.setItem('cadastro', JSON.stringify(json));
+  console.log(localStorage);
+
+  form.reset();
+  document.getElementById('CadastroRealizado').innerHTML = "Cadastro realizado com sucesso!";
+  setTimeout(function () {
+    document.getElementById('CadastroRealizado').innerHTML = '';
+  }, 2000);
+}
